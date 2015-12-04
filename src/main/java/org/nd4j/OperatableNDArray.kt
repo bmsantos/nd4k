d2dd63fec11bb85ee193801cf6094100a0ed44e2
Bruno Santos <bmrosantos@gmail.com>
2015-12-04 09:01:59 -0500

    no need for varargs

--- src/main/java/org/nd4j/OperatableNDArray.kt
+++ src/main/java/org/nd4j/OperatableNDArray.kt
@@ -2,13 +2,17 @@ package org.nd4j
 
 import org.nd4j.linalg.api.ndarray.INDArray
 
-fun INDArray.get<T>(vararg i:Int):T {
-    val j:T? = null;
-    when (j) {
-        is Float -> return getFloat(i[0], if (i.size > 1) i[1] else 0) as T
-        is Double -> return getDouble(i[0], if (i.size > 1) i[1] else 0) as T
+fun INDArray.get<T>(i:Int, j:Int = 0):T {
+    val t:T? = null;
+    when (t) {
+        is Float -> return getFloat(i, j) as T
+        is Double -> return getDouble(i, j) as T
     }
-    return getInt(i[0], if (i.size > 1) i[1] else 0) as T
+    return getInt(i, j) as T
+}
+
+fun INDArray.get<T>(vararg i:Int):Int {
+    return getInt(i[0], if (i.size > 1) i[1] else 0)
 }
 
 fun INDArray.plus(a:Number):INDArray {
