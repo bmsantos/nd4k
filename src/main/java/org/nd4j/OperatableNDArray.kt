4219080f922134564106932d71aad23151f2a7b2
Bruno Santos <bmrosantos@gmail.com>
2015-12-04 09:09:20 -0500

    fixed deprecated type parameter

--- src/main/java/org/nd4j/OperatableNDArray.kt
+++ src/main/java/org/nd4j/OperatableNDArray.kt
@@ -2,17 +2,13 @@ package org.nd4j
 
 import org.nd4j.linalg.api.ndarray.INDArray
 
-fun INDArray.get<T>(i:Int, j:Int = 0):T {
+fun <T> INDArray.get(i:Int, j:Int = 0):T {
     val t:T? = null;
     when (t) {
         is Float -> return getFloat(i, j) as T
         is Double -> return getDouble(i, j) as T
     }
     return getInt(i, j) as T
-}
-
-fun INDArray.get<T>(vararg i:Int):Int {
-    return getInt(i[0], if (i.size > 1) i[1] else 0)
 }
 
 fun INDArray.plus(a:Number):INDArray {
