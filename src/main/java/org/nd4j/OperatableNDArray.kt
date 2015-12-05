1b43f5aea5a853b0f155b2d30b7c8d01a6c882e6
Bruno Santos <bmrosantos@gmail.com>
2015-12-04 23:44:45 -0500

    A wee bit foo issue

--- src/main/java/org/nd4j/OperatableNDArray.kt
+++ src/main/java/org/nd4j/OperatableNDArray.kt
@@ -2,7 +2,11 @@ package org.nd4j
 
 import org.nd4j.linalg.api.ndarray.INDArray
 
-fun <T> INDArray.get(i:Int, j:Int = 0):T {
+public class Foo {
+    public fun hello() = println("Hello World")
+}
+
+public fun <T> INDArray.get(i:Int, j:Int = 0):T {
     val t:T? = null;
     when (t) {
         is Float -> return getFloat(i, j) as T
@@ -11,10 +15,10 @@ fun <T> INDArray.get(i:Int, j:Int = 0):T {
     return getInt(i, j) as T
 }
 
-fun INDArray.plus(a:Number):INDArray {
+public fun INDArray.plus(a:Number): INDArray {
     return this.add(a)
 }
 
-fun INDArray.minus(a:Number):INDArray {
+public fun INDArray.minus(a:Number): INDArray {
     return this.sub(a)
 }
